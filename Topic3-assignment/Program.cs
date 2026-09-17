e3442f2af834e185ff051863d5165182e9ebea93
SAMMY <326589150+sqn10@users.noreply.github.com>
2026-09-17 13:19:35 -0400

    added beginning of program

--- Topic3-assignment/Program.cs
+++ Topic3-assignment/Program.cs
@@ -4,7 +4,20 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Sammy
+
+            int roomNumber = 29;
+            double price = 2.99;
+            string className = "Computer Science";
+            string fullName = "Sammy N.";
+            int graduationYear = 2028;
+
+            Console.WriteLine("This is room # " + roomNumber + ".");
+            Console.WriteLine("The price is " + price.ToString("C") + ".");
+            Console.WriteLine("I am learning a bit about " + className + "!");
+            Console.WriteLine("My name is " + fullName + " and I'll graduate in " + graduationYear + ".");
+
+            //finish rest of program
         }
     }
 }
